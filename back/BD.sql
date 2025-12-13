30eeaa344224910b4b9d2631dbba20dd8b2de251
Valentindg2506 <valentindegennaro@gmail.com>
2025-12-13 13:40:06 +0100

    1

---
+++ back/BD.sql
@@ -0,0 +1,22 @@
+CREATE TABLE usuario (
+  id INT,
+  nombre VARCHAR(255),
+  apellidos VARCHAR(255),
+  correo VARCHAR(255),
+  contrasea VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE contenido (
+  id INT,
+  usuario_id INT,
+  titulo VARCHAR(255),
+  tipo VARCHAR(255),
+  estado VARCHAR(255),
+  puntuacion VARCHAR(255),
+  comentario VARCHAR(255),
+  fecha_visualizacion VARCHAR(255),
+  nivel_prioridad VARCHAR(255),
+  PRIMARY KEY (id),
+  CONSTRAINT fk_contenido_1 FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
