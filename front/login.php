811aa0906c7af24ca2fb7382d4ac016c59346a79
Dani <danieloliveiravidal12@gmail.com>
2025-12-13 14:24:54 +0100

    13/12/2025

---
+++ front/login.php
@@ -0,0 +1,14 @@
+<?php include "inc/cabera.php"; ?>
+<section id="login">
+    <form>
+        <div class="controlsesion">
+            <label for="usuario">Nombre usuario</label>
+            <input type="text" name="usuario" id="usuario">
+        </div>
+        <div class="controlsesion">
+            <label for="contrasena">Contraseña</label>
+            <input type="password" name="contrasena" id="contrasena">
+        </div>
+    </form
+</section>
+<?php include "inc/piedepagina"; ?>
